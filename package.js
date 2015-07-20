7f9342a68cfcf3f3c7f9bc4fa3ecabdaa3e8384b
Pusher Robot <jack@semantic-ui.com>
2015-07-20 18:46:14 -0400

    Updated component to version 2.0.5

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-container',
   summary : 'Semantic UI - Container: Single component release',
-  version : '2.0.4',
+  version : '2.0.5',
   git     : 'git://github.com/Semantic-Org/UI-Container.git',
 });
 
