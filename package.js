0b829b453a515052020064141d21a78ea1ec0671
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:40:53 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-container',
   summary : 'Semantic UI - Container: Single component release',
-  version : '2.2.9',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Container.git',
 });
 
